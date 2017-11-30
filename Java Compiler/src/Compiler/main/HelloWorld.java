163e1fb88ea31a93c392112c894a11752232466a
TerranceKhumalo <khumaloterrance@gmail.com>
2017-11-30 14:18:09 +0200

    the java code had errors so i fix it by adding the package

--- Java Compiler/src/Compiler/main/HelloWorld.java
+++ Java Compiler/src/Compiler/main/HelloWorld.java
@@ -1,6 +1,9 @@
+
+package Compiler.main;
+
 public class HelloWorld{
    
-   public static main void(String..args){
+   public static void main (String...args){
    
    System.out.println("Sho sho Terrance");
    }
