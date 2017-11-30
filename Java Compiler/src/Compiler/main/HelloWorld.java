827972a6fe21ae29e6e7cc9ff298b9cbdaab299b
Lehlogonolo Monini <34057753+lehlogonolo23@users.noreply.github.com>
2017-11-30 14:58:09 +0200

    I added a print line

--- Java Compiler/src/Compiler/main/HelloWorld.java
+++ Java Compiler/src/Compiler/main/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld{
    public static void main (String...args){
    
    System.out.println("Sho sho Terrance");
+   System.out.println("Suggestions on where to look for compiler code or functions?");
    }
 
 }
